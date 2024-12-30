f4444786d44091853dc12f16be9edde247affe96
Adam Rose <93153300+AdamRose1@users.noreply.github.com>
2024-12-30 15:32:22 -0500

    Update Scan_Subnet_Nmap.sh

--- Scan_Subnet_Nmap.sh
+++ Scan_Subnet_Nmap.sh
@@ -21,17 +21,34 @@ else
     printf "Nmap will use default speed scan, it will not add --min-rate=5000.\n\n"
 fi
 
+# Get user input for protocol option
+printf "Do you want to scan UDP or TCP? "
+read protocol
+protocol=$(printf "$protocol" | tr '[:upper:]' '[:lower:]')
+
+# Check what protocol user wants to use
+if [[ "$protocol" == "udp" || "$protocol" == "u" ]]; then
+    protocol="-sU"
+    host_discovery="" 
+    printf "Starting UDP scan.\n\n"
+else
+    protocol=""
+    host_discovery="-sn"
+    printf "Starting TCP scan.\n\n"
+fi
+
 # nmap host discovery 
+# While UDP doesn't have a great way for host discovery, it is still important to run this first scan for UDP, because it will determine if the host is up based on scanning 1000 ports instead of scanning 65k ports on all hosts.  That will save a lot of time.
 mkdir step1_host-discovery && cd step1_host-discovery
-nmap $host_discovery $target $speed -oN nmap_host-discovery 
+sudo nmap $target $host_discovery $speed $protocol -oN nmap_host-discovery 
 
 # nmap scan all 65k ports on every host discovered in the previous command
 mkdir step2_65k-find-open-ports && cd step2_65k-find-open-ports
-for ip in $(cat ../nmap_host-discovery|grep 'scan report' |awk '{print $5}');do nmap -Pn -p- $ip $speed -oN nmap_65k-ports_$ip;done
+for ip in $(cat ../nmap_host-discovery|grep 'scan report' | awk '{print $5}');do sudo nmap -Pn -p- $ip $speed $protocol -oN nmap_65k-ports_$ip;done
 
 # nmap version and script scanning on every open port for each host discovered
 mkdir step3_sCV && cd step3_sCV
-for ip in $(cat ../../nmap_host-discovery|grep 'scan report'|awk '{print $5}');do for ports in $(cat ../nmap_65k-ports_$ip|grep open|awk -F '/' '{print $1}'|sed -z 's/\n/,/g'|sed 's/,$//');do nmap -Pn $ip $speed  -p $ports -sCV -oN nmap_sCV_$ip;done;done
+for ip in $(cat ../../nmap_host-discovery|grep 'scan report'|awk '{print $5}');do for ports in $(cat ../nmap_65k-ports_$ip|grep open|awk -F '/' '{print $1}'|sed -z 's/\n/,/g'|sed 's/,$//');do sudo nmap -Pn $ip $speed $protocol -p $ports -sCV -oN nmap_sCV_$ip;done;done
 
 # Create a directory for each host discovered with open ports, and then move each nmap file output to the target directory.  This is helpful for organizing notes on large subnets
 mkdir all_targets && cd all_targets
@@ -40,5 +57,5 @@ for ip in $(ls ../nmap*|awk -F '_' '{print $3}');do mkdir $ip && cp ../nmap_sCV_
 # Clean up
 cd ../ && mv all_targets ../../../ && cd ../../../ && rm -rf step1_host-discovery
 
-# If you do not want to create the empty files below in each target IP comment the line below
+# If you do not want to create the below empty files in each target IP, then comment out the line below
 for ip in $(ls all_targets);do touch all_targets/$ip/enumeration.txt all_targets/$ip/exploit_path.txt all_targets/$ip/creds.txt;done
